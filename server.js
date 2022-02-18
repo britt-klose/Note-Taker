b93061c5b4c187b2ef3e4ea12d8bac4fac54e909
brittany klose <brittany.klose@yahoo.com>
2022-02-18 14:53:55 -0500

    fized port link and added fs

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs'); //circle back
 const path = require('path');
 const api = require('./routes/apiroutes.js');
 const html = require('./routes/htmlroute.js')
@@ -11,11 +12,12 @@ const app = express();
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.use(express.static('public'))
 app.use('/api', api);
 app.use('/', html);
 
+app.use(express.static('public'));
+
+
 app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
+  console.log(`App listening at https://localhost:${PORT} 🚀`)
 );
