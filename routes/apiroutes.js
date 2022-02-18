a33ddd4764c5b08bd95950d569ed365e38ab85dc
brittany klose <brittany.klose@yahoo.com>
2022-02-18 14:50:41 -0500

    working on get and post requests

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,14 +1,64 @@
 
-const app= require ('express').Router();
+const api= require ('express').Router();
+let newNote = require ('../db/db.json');
+let noteArray
 
-app.get('/api/notes', (req, res) => {
-    console.log('was hit')
+//GET/api/notes should read db.json file and return saved notes as JSON
+api.get('/api/notes', (req, res) => {
+    console.log('was hit');
+    res.json(newNote)
+   //readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
   // res.sendFile(path.join(__dirname, '../db/db.json')) 
 });
 
-app.post('/api/notes', (req, res) =>{
+//GET Route for specific note
+//tutor had me delete .thens before 
+// api.get('/:note_id', (req, res) => {
+//   const noteId =req.params.note_id;
+//   fs.readFile ('./db/db.json')
+//   .then((data) => JSON.parse (data))
+//   .then((json) =>{
+//     const result =json.filter((note) => note.note_id ===noteId);
+//     return result.length>0
+//       ? res.json(result)
+//       : res.json("No notes with that ID");
+//   });
+// });
+
+//DELETE Route for specific note
+//might have to delete .thens
+api.delete('/api/:note_id', (req, res) =>{
+  // const noteId = req.params.note_id;
+  // fs.readFile('./db/db.json')
+  // .then ((data) => JSON.parse(data))
+  // .then ((json) => {
+    //Making a new array for all the notes except the note deleted (w id provided in url)
+    //const result = json.filter((note) => note.note_id !== noteId);
+    //save this array to file system
+    noteArray=[];
+    for (let i = 0; i < newNote.length; i++) {
+      if(req.params.id != newNote[i].id) {
+          noteArray.push(newNote[i])
+      }
+      
+    }
+    fs.writeFileSync('../db/db.json', JSON.stringify(noteArray));
+    newNote = noteArray
+    res.json(newNote);
+  });
+//});
+
+//POST /api/notes should receive new note to save on request body
+//and add it to db.json. THen return new note to client.
+//each note needs unique id when saved. (look into npm packages to help)
+api.post('/api/notes', (req, res) =>{
     console.log(req.body)
+    req.body.id = Math.floor((1+ Math.random())* 0x100000)
+    newNote.push(req.body);
+    fs.writeFileSync('../db/db.json', JSON.stringify(newNote));
+    res.json(newNote);
     
 });
+
    
-module.exports =app
+module.exports =api
