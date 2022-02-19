f126da74fb9115f28eb6a93846578084128042c2
brittany klose <brittany.klose@yahoo.com>
2022-02-19 15:11:58 -0500

    deleted uneeded comments

--- routes/htmlroute.js
+++ routes/htmlroute.js
@@ -1,16 +1,15 @@
+
 const path = require ('path');
-//const app = require('.');
 const html = require ('express').Router();
 
-// GET Route for feedback page
-//GET/notes should return notes.html file
+
+// GET/notes should return notes.html file
 html.get('/notes', (req, res) => 
   res.sendFile(path.join(__dirname, '../public/notes.html'))
 );
 
-// Wildcard route to direct users to a homepage
-//GET * should return index.html file
-//get/ vs get *
+
+// GET / should return index.html file
 html.get('/', (req, res) =>{
   console.log('testing spot');
   res.sendFile(path.join(__dirname, '../public/index.html'))
